4c046ec750854c186552ddad37c377919cad8729
Sudheendra chari <sudheendrachari@users.noreply.github.com>
2026-05-31 16:59:54 +0530

    Add: merge-two-sorted-linked-lists - submission-5

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-5.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} list1
+     * @param {ListNode} list2
+     * @return {ListNode}
+     */
+    mergeTwoLists(list1, list2) {
+        let list3Head = new ListNode();
+        let list3Cur = list3Head;
+        let c1 = list1,
+            c2 = list2;
+        while (c1 !== null && c2 !== null) {
+            if (c1.val <= c2.val) {
+                list3Cur.next = c1;
+                list3Cur = c1;
+                c1 = c1.next;
+            } else {
+                list3Cur.next = c2;
+                list3Cur = c2;
+                c2 = c2.next;
+            }
+        }
+        if (c1) {
+            list3Cur.next = c1;
+        } else {
+            list3Cur.next = c2;
+        }
+        return list3Head.next;
+    }
+}
