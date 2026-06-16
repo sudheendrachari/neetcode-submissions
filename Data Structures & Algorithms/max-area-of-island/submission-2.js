39eb346f030aa4b05ee7624fe01790f5606e860d
Sudheendra chari <sudheendrachari@users.noreply.github.com>
2026-06-16 17:08:41 +0530

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    maxAreaOfIsland(grid) {
+        const visited = new Set();
+
+        const rLen = grid.length,
+            cLen = grid[0].length;
+
+        function getNeighbours(r, c) {
+            return [[r + 1, c],[r - 1, c],[r, c + 1],[r, c - 1]];
+        }
+
+        function bfs(r, c) {
+            const queue = [[r, c]];
+            let area = 0;
+            visited.add(`${r}-${c}`);
+            while (queue.length) {
+                area++;
+                const [row, col] = queue.shift();
+                const neighbours = getNeighbours(row, col);
+                for (const n of neighbours) {
+                    const [nr, nc] = n;
+                    if (nr < 0 || nc < 0) continue;
+                    if (nr > rLen - 1 || nc > cLen - 1) continue;
+                    if (grid[nr][nc] === 0) continue;
+                    if(visited.has(`${nr}-${nc}`)) continue;
+                    queue.push([nr, nc]);
+                    visited.add(`${nr}-${nc}`);
+                }
+            }
+            return area;
+        }
+        let maxArea = 0;
+        for (let i = 0; i < rLen; i++) {
+            for (let j = 0; j < cLen; j++) {
+                if (grid[i][j] === 1 && !visited.has(`${i}-${j}`)) {
+                    maxArea = Math.max( maxArea, bfs(i, j));
+                }
+            }
+        }
+        return maxArea;
+    }
+}
