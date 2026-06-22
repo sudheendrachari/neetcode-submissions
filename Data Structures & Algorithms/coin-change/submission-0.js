16d62f178e7f3ccc101f9290b3f74413d198c5b5
Sudheendra chari <sudheendrachari@users.noreply.github.com>
2026-06-22 21:00:32 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} coins
+     * @param {number} amount
+     * @return {number}
+     */
+    coinChange(coins, amount) {
+        const dp = new Array(amount+1).fill(Infinity);
+        dp[0] = 0;
+
+        for(const coin of coins) {
+            for(let i = coin; i<amount+1; i++) {
+                dp[i] = Math.min(
+                    dp[i],
+                    dp[i - coin] + 1
+                );
+            }
+        }
+        return dp[amount] === Infinity ? -1 : dp[amount];
+    }
+}
