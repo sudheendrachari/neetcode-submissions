706341d04a8204cccb6541071aa9447a177f27d3
Sudheendra chari <sudheendrachari@users.noreply.github.com>
2026-07-03 15:04:37 +0530

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        const sorted = intervals.sort((a,b) => a[0] - b[0]);
+        const res = [];
+        let prev = sorted[0];
+        for(let i = 1; i < sorted.length; i++) {
+            const cur = sorted[i];
+            let [prevStart, prevEnd] = prev;
+            let [curStart, curEnd] = cur;
+            if(prevEnd < curStart) {
+                res.push(prev);
+                prev = cur;
+            } else {
+                prev = [Math.min(prevStart, curStart), Math.max(prevEnd, curEnd)];
+            }
+
+        }
+        if(prev) res.push(prev);
+        return res;
+    }
+}
