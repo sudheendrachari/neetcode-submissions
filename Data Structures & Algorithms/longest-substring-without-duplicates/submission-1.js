9507018526e55a85d1968e10e51dc24c7df30b69
Sudheendra chari <sudheendrachari@users.noreply.github.com>
2026-09-13 10:37:48 +0530

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let left = 0, max = 0;
+        const seen = new Set();
+        for(let right = 0; right < s.length; right++) {
+            while(seen.has(s[right])) {
+                seen.delete(s[left]);
+                left++;
+            }
+            seen.add(s[right]);
+            max = Math.max(max, seen.size);
+        }
+        return max;
+    }
+}
